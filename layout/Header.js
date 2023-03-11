754a3fa1835b3d2c157bf4f6b5ffa11ae533140d
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-11 23:19:27 +0330

    Complete header #1

--- layout/Header.js
+++ layout/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 	return (
 		<section className="container flex items-center justify-between py-5 md:justify-start">
 			<div className="flex md:hidden">
-				<CiMenuBurger size={"2rem"} />
+				<CiMenuBurger size={"1.8rem"} />
 			</div>
 
 			<div className="hidden md:flex ml-24 lg:ml-48">
