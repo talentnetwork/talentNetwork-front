bf3856e8a0b55e2d9d9f003804c9a022cedd3ef7
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-11 22:55:32 +0330

    responsive Navbar Component

--- layout/Header.js
+++ layout/Header.js
@@ -3,12 +3,12 @@ import { CiMenuBurger } from "react-icons/ci";
 
 const Header = () => {
 	return (
-		<section className="container flex items-center justify-between">
+		<section className="container flex items-center justify-between md:justify-start">
 			<div className="flex md:hidden">
 				<CiMenuBurger />
 			</div>
 
-			<div className="hidden md:flex">
+			<div className="hidden md:flex py-5 ml-24 lg:ml-48">
 				<NavbarHeader />
 			</div>
 		</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,54 @@
 module.exports = {
-    content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./layout/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
 
-  theme: {
-    fontFamily:{
-      serif:['Poppins'],
-      sans:['Poppins'],
-      desplay:['Poppins'],
-      body:['Poppins'],
-      mono:['Poppins'],
-    },
-    extend: {
-      colors:{
-        primary:{
-          100:'#FFF4EE',
-          200:'#FFC29D',
-          300:'#9C5A35',
-          400:'#4E2D1A',
-        },
-        secondary:{
-          100:'#DDEBFF',
-          200:'#90B9F6',
-          300:'#355E9C',
-          400:'#1A2F4E',
-        },
-        typoDark:{
-          100:'#BABABA',
-          200:'#989898',
-          300:'#757575',
-          400:'#535353',
-        },
-        success:{
-          100:'#50CB82',
-        },
-        error:{
-          100:'#E56857',
-        },
-        warning:{
-          100:'#FFDB7D',
-        },
-      }
-    },
-  },
-  plugins: [],
-}
+	theme: {
+		fontFamily: {
+			serif: ["Poppins"],
+			sans: ["Poppins"],
+			desplay: ["Poppins"],
+			body: ["Poppins"],
+			mono: ["Poppins"],
+		},
+		container: {
+			center: true,
+			padding: "1rem",
+		},
+		extend: {
+			colors: {
+				primary: {
+					100: "#FFF4EE",
+					200: "#FFC29D",
+					300: "#9C5A35",
+					400: "#4E2D1A",
+				},
+				secondary: {
+					100: "#DDEBFF",
+					200: "#90B9F6",
+					300: "#355E9C",
+					400: "#1A2F4E",
+				},
+				typoDark: {
+					100: "#BABABA",
+					200: "#989898",
+					300: "#757575",
+					400: "#535353",
+				},
+				success: {
+					100: "#50CB82",
+				},
+				error: {
+					100: "#E56857",
+				},
+				warning: {
+					100: "#FFDB7D",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
