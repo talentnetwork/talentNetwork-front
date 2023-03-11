edc1b61b42804d39c93dcc14c540b7c5fdd96fa9
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-03-11 21:59:59 +0330

    config tailwind - add base font & color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,49 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    
   ],
 
   theme: {
-    extend: {},
+    fontFamily:{
+      serif:['Poppins'],
+      sans:['Poppins'],
+      desplay:['Poppins'],
+      body:['Poppins'],
+      mono:['Poppins'],
+    },
+    extend: {
+      colors:{
+        primary:{
+          100:'#FFF4EE',
+          200:'#FFC29D',
+          300:'#9C5A35',
+          400:'#4E2D1A',
+        },
+        secondary:{
+          100:'#DDEBFF',
+          200:'#90B9F6',
+          300:'#355E9C',
+          400:'#1A2F4E',
+        },
+        typoDark:{
+          100:'#BABABA',
+          200:'#989898',
+          300:'#757575',
+          400:'#535353',
+        },
+        success:{
+          100:'#50CB82',
+        },
+        error:{
+          100:'#E56857',
+        },
+        warning:{
+          100:'#FFDB7D',
+        },
+      }
+    },
   },
   plugins: [],
 }
